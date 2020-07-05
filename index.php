11e797a262cea4aac545718669248d2014a4e0ba
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-07-05 18:39:43 +0700

    Init source

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+  <script src="/public/js/jquery-3.5.1.min.js"></script>
+  <script src="/public/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!!";
+?> 
+
+</body>
+</html>
