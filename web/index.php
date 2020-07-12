ea3fc16ada50d282b5626fdcb52e1b36861abc4b
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-07-12 16:54:19 +0700

    update components UI base on tachyons

--- web/index.php
+++ web/index.php
@@ -6,30 +6,33 @@
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
   <link rel='stylesheet' href='/css/libs/tachyons.min.css'>
+  <script type='text/javascript' src='/js/libs/jquery.min.js'></script>
+  <script type='text/javascript' src='/js/libs/jquery-qrcode.min.js'></script>
+  <script type='text/javascript' src='/js/utils/qr_code.js'></script>
 </head>
 
 <body>
-  <div class='w-80'>
-    <div id='qrcode-img' class='p-5' style='height: 300px'>
+  <div class=''>
+    <div id='qrcode-img'>
       <img src='' />
     </div>
     <div id='qrcode-title'></div>
-    <div class='w-full px-5'>
-      <div class='py-2 flex justify-between leading-8'>
-        <div class='text-sm font-bold'>Choose Input Type</div>
-        <select id='select-input-type' class='outline-none bg-gray-300 p-2 rounded-l text-sm'>
-          <option value='url' class='text-sm' selected>Url</option>
-          <option value='text' class='text-sm'>Text</option>
-          <option value='phone' class='text-sm'>Phone</option>
+    <div class=''>
+      <div class='flex-ns justify-between-ns mv2'>
+        <div class='b'>Choose Input Type</div>
+        <select id='select-input-type' class='bg-gray-05 pa1'>
+          <option value='url' class='' selected>Url</option>
+          <option value='text' class=''>Text</option>
+          <option value='phone' class=''>Phone</option>
         </select>
       </div>
-      <textarea id='qrcode-text' class='w-full h-24 bg-gray-200 rounded-md p-3 min-h-12' value=''></textarea>
-      <div id='input-error' class='hidden text-red-600'>Phone number invalid!</div>
+      <textarea id='qrcode-text' class='bg-gray-05 br2 pa2' value=''></textarea>
+      <div id='input-error' class='red dn'>Phone number invalid!</div>
     </div>
-    <div class='w-full my-5 text-center'>
-      <button id='btn_save_qr' class="focus:outline-none bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-        <span>Download</span>
+    <div class=''>
+      <button id='btn_save_qr' class='b b-0 grow inline-flex items-center ma2 no-underline pa2 tc'>
+        <svg class='dib h1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path d='M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z'/></svg>
+        <span class='f6 ml3 pr2'>Download</span>
       </button>
     </div>
   </div>
@@ -38,11 +41,24 @@
 
   ?>
   <style>
-
+    .b-0 {
+      border: 0;
+    }
+    .bg-gray { background-color: #777; }
+    .bg-gray-90 { background-color: rgba( 0, 0, 47, .9 ); }
+    .bg-gray-80 { background-color: rgba( 0, 0, 47, .8 ); }
+    .bg-gray-70 { background-color: rgba( 0, 0, 47, .7 ); }
+    .bg-gray-60 { background-color: rgba( 0, 0, 47, .6 ); }
+    .bg-gray-50 { background-color: rgba( 0, 0, 47, .5 ); }
+    .bg-gray-40 { background-color: rgba( 0, 0, 47, .4 ); }
+    .bg-gray-30 { background-color: rgba( 0, 0, 47, .3 ); }
+    .bg-gray-20 { background-color: rgba( 0, 0, 47, .2 ); }
+    .bg-gray-10 { background-color: rgba( 0, 0, 47, .1 ); }
+    .bg-gray-05 { background-color: rgba( 0, 0, 47, .05 ); }
+    #qrcode-img {
+      height: 300px;
+    }
   </style>
-  <script type='text/javascript' src='/js/libs/jquery.min.js'></script>
-  <script type='text/javascript' src='/js/libs/jquery-qrcode.min.js'></script>
-  <script type='text/javascript' src='/js/utils/qr_code.js'></script>
-  <script type='text/javascript' src='/js/home.js' defer></script>
+  <script type='text/javascript' src='/js/home.js'></script>
 </body>
 </html>
