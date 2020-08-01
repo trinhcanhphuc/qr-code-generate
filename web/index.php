72d93e2b7ace7a9e1cc5660df27890163b8b1f0f
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-08-01 23:46:05 +0700

    Update header

--- web/index.php
+++ web/index.php
@@ -10,17 +10,25 @@
 </head>
 
 <body class='mb-20'>
-  <div class='header flex justify-between p-3 shadow-lg sticky inset-x-0 top-0 left-0 bg-white z-10'>
+  <nav class='header flex items-center justify-between flex-wrap bg-white p-6 shadow-lg sticky inset-x-0 top-0 left-0 z-10'>
     <div class='logo flex'>
       <img src='/images/logo.png' class='h-10' />
       <span class='leading-10 font-bold'>QR Code Generator</span>
     </div>
-    <div class='menu-bar flex'>
-      <a href='#' class='leading-10 text-blue-500 font-bold mx-3'>QR Generator</a>
-      <a href='#' class='leading-10 font-bold mx-3'>QR Scanner</a>
-      <a href='#' class='mx-3 text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white cursor-pointer'>Try it free</a>
+    <div class='block lg:hidden'>
+      <button class='flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white'>
+      <i class='fas fa-bars'></i>
+      </button>
     </div>
-  </div>
+    <div class='text-sm flex lg:show'>
+      <a href='#' class='block mt-4 lg:inline-block lg:mt-0 hover:text-blue-200 mr-4 leading-10 text-blue-500 font-bold'>
+        QR Code Generator
+      </a>
+      <a href='#' class='block mt-4 lg:inline-block lg:mt-0 hover:text-blue-200 mr-4 leading-10 text-blue-500 font-bold'>
+        QR Scanner
+      </a>
+    </div>
+  </nav>
   <div class='content w-100 h-100'>
     <div class='introduction text-center mt-16'>
       <h1 class='text-5xl font-boldm'>
@@ -84,7 +92,7 @@
       <div class='w-2/5 center flex'>
       </div>
     </div>
-    <div class='qr-content-section flex m-10 rounded-lg shadow-2xl p-10 mt-0'>
+    <div class='qr-content-section flex lg:m-10 lg:p-10 m-3 mt-0 p-3 rounded-lg shadow-2xl sm:m-5 sm:p-5'>
       <div class='w-3/5'>
         <div id='qrcode-title'></div>
         <div>
