02619f3bcedbb5525c85dad44c0e920bc79b550b
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-11-19 20:17:30 +0700

    Fix npm build error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import Vuetify from "vuetify";
 import { router } from "./routes";
 import * as VeeValidate from "vee-validate";
-import App from './App';
 
 window.Vue = require("vue");
 Vue.use(Vuetify);
@@ -11,7 +10,6 @@ Vue.use(VeeValidate);
 const app = new Vue({
   el: "#app",
   router,
-  render: h => h(App),
   vuetify: new Vuetify({
     icons: {
       iconfont: "fa" // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
