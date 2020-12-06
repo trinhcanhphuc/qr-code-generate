b4b44d26b8bb72ea4c5be730c0d8f9bc08416fc7
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-12-06 15:21:06 +0700

    Add Google Tag Manager

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,13 @@
   <title>QR Code Generator</title>
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PSRP9PQ');</script>
+  <!-- End Google Tag Manager -->
 
 </head>
 
@@ -41,6 +48,10 @@
   -->
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSRP9PQ"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <noscript>
     <strong>We're sorry but this app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
   </noscript>
